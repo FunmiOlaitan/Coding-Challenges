38d9a2926d49bbd60d7c27cf07a0e601b4ae570b
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2024-02-17 14:48:11 +0000

    Subject: Implement credit card length, vendor validation and a main function to credit_card_validator.py
    Body:
    Implemented function to validate credit card length and ensure the credit card number meets the required length (13 to 16 digits).
    Implemented function to identify the vendor based on the card number prefix.
    Created the main function to validate the credit card number.

--- CreditCardValidator/credit_card_validator.py
+++ CreditCardValidator/credit_card_validator.py
@@ -1,11 +1,6 @@
-def luhn_algo(number):
+def validate_luhn_algo(credit_card_number):
     # converts each digit in the string into a int and store in list 
-    digits = []
-
-    for num in number:
-        digit = int(num)
-        digits.append(digit)
-    
+    digits = [int(num) for num in credit_card_number]
     checksum = 0
 
     for i in range(len(digits) -2, -1, -2):
@@ -15,3 +10,49 @@ def luhn_algo(number):
     
     checksum = sum(digits) % 10
     return checksum == 0
+
+def validate_credit_card_length(credit_card_number):
+    card_number = ''.join(char for char in credit_card_number if char.isdigit())
+
+    card_length = len(card_number)
+    if 13 <= card_length <= 16:
+        return True 
+    else:
+        return False 
+    
+def validate_credit_card_vendor(credit_card_number):
+    # This line of code removes any non-digit characters before checking vendor
+    card_number =  ''.join(char for char in credit_card_number if char.isdigit())
+
+    if card_number.startswith('34') or card_number.startswith ('37'):
+        return "American Express"
+    elif 51 <= int(card_number[:2]) <= 55:
+        return "Mastercard"
+    elif card_number.startswith('4'):
+        return "Visa"
+    elif card_number.startswith('6011'):
+        return "Discover"
+    else:
+        return None
+
+def main():
+    credit_card_number = input("Enter credit card number: ")
+
+    if not validate_credit_card_length(credit_card_number):
+        print("Invalid credit card number length.")
+        return
+
+    vendor = validate_credit_card_vendor(credit_card_number)
+    if vendor:
+        print("Credit card vendor:", vendor)
+    else:
+        print("Unknown credit card vendor.")
+        return
+
+    if validate_luhn_algo(credit_card_number):
+        print("Valid credit card number.")
+    else:
+        print("Invalid credit card number.")
+
+if __name__ == "__main__":
+    main()
