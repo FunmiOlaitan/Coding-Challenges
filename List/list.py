f665894e2ab86610fd17f5b01b5dca57c8630df9
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2024-03-07 19:03:57 +0000

    Implement methods to extract and sort lists of lists in list.py

--- List/list.py
+++ List/list.py
@@ -12,4 +12,16 @@ class ListManager:
     def inserting_element(self, position, element):
         self.my_list[position:position] = [element]
         return self.my_list
+    
+    def extract_list_of_list(self, input_list, num_list, num_elements):
+        list_of_list = []
+        for _ in range(num_list):
+            random_elements = random.sample(input_list, num_elements)
+            list_of_list.append(random_elements)
+        return list_of_list
+    
+    def sort_list_of_list(self, list_of_lists):
+        return sorted(list_of_lists)
+
+if __name__ == "__main__":
     pass
