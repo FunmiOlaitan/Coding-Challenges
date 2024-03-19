e17bb7df14e04dd5b55aeaaf20f5384e50ceafc4
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2024-03-19 21:09:58 +0000

    Add initial skeleton for Triangulate class in triangulate.py

---
+++ Triangulate/triangulate.py
@@ -0,0 +1,17 @@
+class Triangulate:
+    def __init__(self):
+        self.first_side = None
+        self.second_side = None
+        self.third_side = None
+    
+    def get_dimensions(self):
+        pass
+    
+    def validate_triangle(self):
+        pass
+
+    def classify_triangle(self):
+        pass
+
+    def calculate_missing_side(self, angle):
+        pass
