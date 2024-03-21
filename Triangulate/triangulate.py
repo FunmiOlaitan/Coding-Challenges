0858aaa0fbc59cb071004dbfc64271f5000e3463
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2024-03-21 08:58:54 +0000

    Implement get_dimensions method in triangulate.py

--- Triangulate/triangulate.py
+++ Triangulate/triangulate.py
@@ -5,7 +5,18 @@ class Triangulate:
         self.third_side = None
     
     def get_dimensions(self):
-        pass
+        print("Please enter the length for all three sides of a triangle:")
+        while True:
+            try:
+                self.first_side = float(input("Please enter the length of the first side if the triangle: "))
+                self.second_side = float(input("Please enter the length of the second side of the triangle: "))
+                self.third_side = float(input("Please enter the length of the third side of the triangle: "))
+                if self.first_side <= 0 or self.second_side <= 0 or self.third_side <=0:
+                    print("Length must be positive numbers. Please try again.")
+                else:
+                    break
+            except ValueError:
+                print("Invalid input. Please enter numerical values")
     
     def validate_triangle(self):
         pass
